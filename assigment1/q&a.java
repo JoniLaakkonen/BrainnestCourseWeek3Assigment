a7692fbe206c4590c4b2ea13cccc68822796528a
JoniLaakkonen <89970474+JoniLaakkonen@users.noreply.github.com>
2022-07-21 19:58:18 +0300

    Add files via upload

---
+++ assigment1/q&a.java
@@ -0,0 +1,149 @@
+
+1. Name the three ways to declare a variable?
+
+var, let, const
+
+2. Which of the three variable declarations should you avoid and why?
+
+var, it will not be supported in the future
+
+3. What rules should you follow when naming variables?
+
+The name must contain only letters, digits, or the symbols $ and _
+The first character must not be a digit.
+
+4. What should you look out for when using the + operator with numbers and
+strings?
+
+It can mean two different things, string concatenation or numeric addition. This is, by definition, an operator overload.
+
+5. How does the % operator work?
+
+divides
+
+6. Explain the difference between == and ===.
+
+== operator does the type conversion of the operands before comparison, whereas the === operator compares the values as well as the data types of the operands
+
+7. When would you receive a NaN result?
+
+when the value cannot be computed or as a result of attempted number coercion for which primitive numeric value is not available
+
+8. How do you increment and decrement a number?
+
+variable--;
+( -- ) that decrements
+variable++;
+( ++ ) increments
+
+9. Explain the difference between prefixing and post-fixing increment/decrement
+operators.
+
+
+
+10. What is operator precedence and how is it handled in JS?
+
+
+
+11. How do you log information to the console?
+
+
+
+12. What does unary plus operator do to string representations of integers?
+
+
+
+13. What are the eight data types in JavaScript?
+
+
+
+14. Which data type is NOT primitive?
+
+
+
+15. What is the relationship between null and undefined?
+
+
+
+16. What is the difference between single, double, and backtick quotes for strings?
+
+
+
+17. What is the term for embedding variables/expressions in a string?
+
+
+
+18. Which type of quote lets you embed variables/expressions in a string?
+
+
+
+19. How do you embed variables/expressions in a string?
+
+
+
+20. How do you escape characters in a string?
+
+
+
+21. What is the difference between the slice/substring/substr string methods?
+
+
+
+22. What are the three logical operators and what do they stand for?
+
+
+
+23. What are the comparison operators?
+
+
+
+24. What are truthy and falsy values?
+
+
+
+25. What are the falsy values in JavaScript?
+
+
+
+26. What are conditionals?
+
+
+
+27. What is the syntax for an if/else conditional?
+
+
+
+28. What is the syntax for a switch statement?
+
+
+
+29. What is the syntax for a ternary operator?
+
+
+
+30. What is nesting?
+
+
+
+31. What are functions useful for?
+
+
+
+32. How do you invoke a function?
+
+
+
+33. What are anonymous functions?
+
+
+
+34. What is function scope?
+
+
+
+35. What are return values?
+
+
+
+36. What are arrow functions?
+
